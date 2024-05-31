6a262368cc180a03c9577c42580b49f464ad673c
Agheroo <reauxrenan@outlook.fr>
2024-05-31 13:31:11 +0200

    Added front

--- routes/puzzles.js
+++ routes/puzzles.js
@@ -7,9 +7,19 @@ router.get('/', function(req, res, next) {
   {app_name : "[Logs-it]",
     pages : [{name: "Puzzles", desc: "Apprenez-en plus sur les performances des utilisateurs sur le puzzle de votre choix !", link: "/puzzles"},
             {name: "Users", desc: "Vous êtes curieux du travail qu'a produit un utilisateur en particulier ?", link: "/users"},
-            {name: "Leaderboard", desc: "Envie de faire partie des meilleurs ? Regardez où vous en êtes dans le classement !", link: "/leaderboard"}]
-                      
-    });
+            {name: "Leaderboard", desc: "Envie de faire partie des meilleurs ? Regardez où vous en êtes dans le classement !", link: "/leaderboard"}],
+
+    //This list should be updated with fetching database accoring to form GET
+    puzzles : [{topic : "Linux", avgScore : 365, difficulty : 0},
+               {topic : "Linux", avgScore : 280, difficulty : 1}, 
+               {topic : "Prog C", avgScore : 378, difficulty : 0},
+               {topic : "Prog C", avgScore : 123, difficulty : 2},
+               {topic : "Prog C", avgScore : 450, difficulty : 1},
+               {topic : "Application web", avgScore : 652, difficulty : 0},
+               {topic : "Application web", avgScore : 51, difficulty : 2}]             
+    
+  
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,14 @@ router.get('/', function(req, res, next) {
   {app_name : "[Logs-it]",
   pages : [{name: "Puzzles", desc: "Apprenez-en plus sur les performances des utilisateurs sur le puzzle de votre choix !", link: "/puzzles"},
           {name: "Users", desc: "Vous êtes curieux du travail qu'a produit un utilisateur en particulier ?", link: "/users"},
-          {name: "Leaderboard", desc: "Envie de faire partie des meilleurs ? Regardez où vous en êtes dans le classement !", link: "/leaderboard"}]
-                      
-
+          {name: "Leaderboard", desc: "Envie de faire partie des meilleurs ? Regardez où vous en êtes dans le classement !", link: "/leaderboard"}],    
+  users : [{name : "Player664", avgScore : 642, rank : 2},
+          {name : "Player7", avgScore : 134, rank : 6},
+          {name : "Player1", avgScore : 345, rank : 3},
+          {name : "Player956", avgScore : 846, rank : 1},
+          {name : "Player12", avgScore : 210, rank : 5},
+          {name : "Player9", avgScore : 320, rank : 4},
+  ]
   });
 });
 
